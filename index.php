6a12a9dab071951b1a24d92696b3dd276bf4767c
Renier Veldman <renierveldman@gmail.com>
2022-06-08 19:52:24 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,79 @@
+﻿<!DOCTYPE html>
+<html lang="en-US" class="no-js">
+  <head>
+
+    <!-- Title -->
+    <title>Clay Digital - Domain Registered</title>
+
+    <!-- Required Meta Tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Bootstrap HTML template">
+
+    <!-- Libs CSS -->
+    <link rel="stylesheet" href="assets/vendor/bootstrap/dist/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="assets/vendor/font-awesome/css/all.min.css" type="text/css">
+    <link rel="stylesheet" href="assets/vendor/themify-icons/css/themify-icons.css" type="text/css">
+    <link rel="stylesheet" href="assets/vendor/slick/slick.min.css" type="text/css">
+    <link rel="stylesheet" href="assets/vendor/featherlight/featherlight.min.css" type="text/css">
+    <link rel="stylesheet" href="assets/vendor/featherlight/featherlight.gallery.min.css" type="text/css">
+    <link rel="stylesheet" href="assets/vendor/aos/dist/aos.css" type="text/css">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&family=Nunito+Sans&display=swap" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link rel="stylesheet" href="assets/css/theme.css">
+    <link rel="stylesheet" href="assets/css/utilities.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="assets/css/custom.css">
+
+  </head>
+  <body>
+
+    <!-- Loader -->
+    <div class="loader bg-dark">
+      <div class="spinner-grow text-primary" role="status">
+        <span class="sr-only">Loading....</span>
+      </div>
+    </div>
+
+    <section class="py-0">
+      <div class="container min-vh-100 d-flex flex-column justify-content-between text-center py-7 py-md-8">
+        <a href="index.html">
+          <img src="assets/images/clay-logo.svg" alt="">
+        </a>
+        <div class="my-5">
+          <h1 class="clay-title">Website Coming Soon</h1>
+          <h3 class="clay-subtitle">This domain has been reserved for a client</h3>
+          <a href="https://claydigital.co.za" class="btn btn-primary mr-2">Visit Our Website</a>
+          <a href="https://facebook.com/claydigitalsa" class="btn btn-soft-secondary">Facebook</a>
+        </div>
+        <div>
+          <p class="clay-footer">© <?php echo date("Y"); ?> Clay Digital (Pty) Ltd - All Rights Reserved - <a href="https://claydigital.co.za/privacy-policy" class="clay-link">Privacy Policy</a></p>
+        </div>
+      </div>
+    </section>
+
+    <!-- Libs JS -->
+    <script src="assets/vendor/jquery/dist/jquery.min.js"></script>
+    <script src="assets/vendor/popper.js/dist/popper.min.js"></script>
+    <script src="assets/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="assets/vendor/jquery-validation/dist/jquery.validate.min.js"></script>
+    <script src="assets/vendor/jquery-form/dist/jquery.form.min.js"></script>
+    <script src="assets/vendor/featherlight/featherlight.min.js"></script>
+    <script src="assets/vendor/featherlight/jquery.detect_swipe.js"></script>
+    <script src="assets/vendor/featherlight/featherlight.gallery.min.js"></script>
+    <script src="assets/vendor/jquery.scrollTo/jquery.scrollTo.min.js"></script>
+    <script src="assets/vendor/jQuery.countdown/dist/jquery.countdown.min.js"></script>
+    <script src="assets/vendor/typed.js/typed.min.js"></script>
+    <script src="assets/vendor/slick/slick.min.js"></script>
+
+    <!-- Theme JS -->
+    <script src="assets/js/main.js"></script>
+
+  </body>
+</html>
